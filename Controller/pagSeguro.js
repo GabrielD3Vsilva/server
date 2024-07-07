e0be42b689a014363d03ab87ce58c61f9d1f4ea7
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-07-07 17:10:14 -0300

    update2

--- Controller/pagSeguro.js
+++ Controller/pagSeguro.js
@@ -1,4 +1,5 @@
 const { MercadoPagoConfig, Preference } = require('mercadopago');
+const db = require('../Db/db');
 
 async function AcessVip(req, res) {
   const {email} = req.body;
@@ -29,6 +30,17 @@ async function AcessVip(req, res) {
 async function consult (req, res) {
   const {idClient, idProfissional} = req.body;
   console.log(idClient, idProfissional);
+
+  const profissional = await db.User.find({_id: idProfissional});
+  let nameProfissional;
+
+  if(profissional.length > 0) {
+    nameProfissional = profissional[0].name;
+    console.log(nameProfissional);
+  }
+
+
+
   const client = new MercadoPagoConfig({ accessToken: "APP_USR-1767806761428068-070620-771a230aa8ff67512387deefe1bd14ef-192552961"});
     
   const preference = new Preference(client);
@@ -37,7 +49,7 @@ async function consult (req, res) {
       items: [
           {
           id: '1',
-          title: 'consulta',
+          title: 'consulta com: '+ nameProfissional,
           quantity: 1,
           currency_id: 'BRL',
           unit_price: 50,
