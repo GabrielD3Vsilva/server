26411be87b65fe15788a59e5d1dce024e638599e
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-07-12 21:44:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const routes = require('./Routes/routes');
 const app = express( );
 
 
+app.use(cors( ));
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
