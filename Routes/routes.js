c42118ef257baaefe92efd2540d95601e37ffd1f
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-07-13 18:48:10 -0300

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -83,12 +83,12 @@ routes.post('/add', async (req, res) => {
             await db.User.updateOne({ _id: adm[i].id }, { $push: { list: idProfissional } });
 
             let transporter = nodemailer.createTransport({
-                service: 'gmail',
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true,
                 auth: {
                     user: 'diasemterapia@gmail.com',
-                    pass: '1981abcd.'
-                },tls: {
-                    rejectUnauthorized: false
+                    pass: 'jqzq jool jevu kexn'
                 }
             });
             
@@ -120,12 +120,12 @@ routes.post('/add', async (req, res) => {
 
 
             let transporter = nodemailer.createTransport({
-                service: 'gmail',
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true,
                 auth: {
                     user: 'diasemterapia@gmail.com',
-                    pass: '1981abcd.'
-                },tls: {
-                    rejectUnauthorized: false
+                    pass: 'jqzq jool jevu kexn'
                 }
             });
             
